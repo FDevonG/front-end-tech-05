9fb175a894abd43be16d113b1a657b86488673d6
Devon Gillis <devongillis88@gmail.com>
2022-07-20 13:12:32 -0300

    Final project upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,48 @@
-const search = new Filter('search', 'data-caption');
+const searchField = document.querySelector("#search");
+const imgAnchors = document.querySelectorAll(".gallery a");
 
 window.addEventListener('load', function() {
   baguetteBox.run('.gallery');
 });
 
+searchField.addEventListener('input', () => {
+  if(searchField.value.trim() === ""){
+    TurnAllImagesOn();
+  }
+  else {
+    SearchImages();
+  }
+});
+
+/*
+This function is called from the search input. it chercks the text input by the user against the data-captions in the image gallery.
+if the search term is found in the data caption then the image visibility is turned on, if its not then its turned off
+*/
+function SearchImages() {
+  const userSearch = searchField.value.trim();
+	for (let i = 0; i < imgAnchors.length; i++) {
+		if (imgAnchors[i].getAttribute('data-caption') !== null){
+			if (!imgAnchors[i].getAttribute('data-caption').toUpperCase().includes(userSearch.toUpperCase())){
+        TurnImageOff(imgAnchors[i]);
+      }
+      else {
+        TurnImageOn(imgAnchors[i]);
+      }
+		}
+	}
+}
+
+//this turns all of the images back on in the event that someone clears the input
+function TurnAllImagesOn (){
+  for (let i = 0; i < imgAnchors.length; i++) {
+    TurnImageOn(imgAnchors[i]);
+  }
+}
+
+function TurnImageOff(element){
+  element.style.display = "none";
+}
+
+function TurnImageOn(element){
+  element.style.display = "inline-block";
+}
